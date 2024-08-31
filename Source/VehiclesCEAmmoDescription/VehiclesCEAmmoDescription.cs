0f5a4f4b3012615af45f3ec76853ced73e1b5c5a
mitasamodel <mitasamodel@gmail.com>
2024-08-31 14:48:51 +0200

    Cleanup, rewrite some methods

--- Source/VehiclesCEAmmoDescription/VehiclesCEAmmoDescription.cs
+++ Source/VehiclesCEAmmoDescription/VehiclesCEAmmoDescription.cs
@@ -26,24 +26,16 @@ namespace VehiclesCEAmmoDescription
 			string logFile = @Environment.CurrentDirectory + @"\Mods\CEAmmoDescription_log.txt";
 			StringBuilder log = new StringBuilder();
 
-			//Turret dictionary: ["defName"] = ammoSet
-			var turretsAmmoSet = TurretsAmmoSetDict();
-			if (DEBUG) LogTurrets(log, turretsAmmoSet);
-
-			//Upgrades dictionary: ["defName"] = UpgradeTreeDef
-			var upgradesDict = UpgradesSetDict();
-			if (DEBUG) LogUpgrades(log, upgradesDict);
-
 			//Go through all "VehicleDef" in the game. Looking for Turrets and Upgrades (may contain turrets)
 			foreach (VehicleDef vehicle in DefDatabase<VehicleDef>.AllDefsListForReading)
 			{
 				log.Append("Vehicle: " + vehicle.defName + "\n");
 
 				//Turrets
-				HyperlinksTurrets(vehicle, turretsAmmoSet, log);
+				LinkTurrets(vehicle, log);
 
 				//Upgrades
-				HyperlinksUpgrades(vehicle, upgradesDict, turretsAmmoSet);
+				LinkUpgrades(vehicle, log);
 
 				log.Append("\n");
 			}
@@ -54,42 +46,27 @@ namespace VehiclesCEAmmoDescription
 		/// <summary>
 		/// <c>HyperlinksUpgrades</c> adds hyperlinks based on vehicle's upgrades
 		/// </summary>
-		private static void HyperlinksUpgrades(VehicleDef vehicle, Dictionary<string, UpgradeTreeDef> upgradesDict, Dictionary<string, AmmoSetDef> turretsAmmoSet, StringBuilder log = null)
+		private static void LinkUpgrades(VehicleDef vehicle, StringBuilder log = null)
 		{
+			//Vehicle has upgrades
 			if (vehicle.GetCompProperties<CompProperties_UpgradeTree>() is CompProperties_UpgradeTree compUpgrades)
 			{
-				log?.Append("Upgrade: " + compUpgrades.def + "\n");
+				log?.AppendLine("Upgrade: " + compUpgrades.def);
 
-				UpgradeTreeDef upgradeTree = new UpgradeTreeDef();
-				upgradeTree = upgradesDict[compUpgrades.def.ToString()];
-				if (upgradeTree == null)
+				UpgradeTreeDef upgradeTree = compUpgrades.def as UpgradeTreeDef;
+
+				//Each upgrade Tree can have multiple upgrade nodes
+				foreach (UpgradeNode node in upgradeTree.nodes)
 				{
-					log?.Append("No upgrade found\n");
-				}
-				else
-				{
-					//Each upgrade Tree can have multiple upgrade nodes
-					foreach (UpgradeNode node in upgradeTree.nodes)
+					//Each node can have several actual upgrades...
+					foreach (Upgrade upgrade in node.upgrades)
 					{
-						//Each node can have several actual upgrades...
-						foreach (Upgrade upgrade in node.upgrades)
+						//But we want turrets only
+						if (upgrade is TurretUpgrade upgTurrets)
 						{
-							//But we want turrets only
-							if (upgrade is TurretUpgrade upgTurrets)
-							{
-								foreach (VehicleTurret turret in upgTurrets.turrets)
-								{
-									log?.Append("UPG Turret: " + turret.turretDef);
-
-									//ammoSet exists for the turret
-									if (turretsAmmoSet[turret.turretDef.ToString()] != null)
-									{
-										log?.AppendLine(" - " + turretsAmmoSet[turret.turretDef.ToString()]);
-
-										AddHyperlinkAmmoSet(vehicle, turretsAmmoSet[turret.turretDef.ToString()], log);
-									}
-								}
-							}
+							//Single upgrade can modify several turrets
+							foreach (VehicleTurret turret in upgTurrets.turrets)
+								LinkTurret(vehicle, turret.turretDef, log);
 						}
 					}
 				}
@@ -99,119 +76,41 @@ namespace VehiclesCEAmmoDescription
 		/// <summary>
 		/// <c>HyperlinksTurrets</c> adds hyperlinks for vehicle's turrets
 		/// </summary>
-		private static void HyperlinksTurrets(VehicleDef vehicle, Dictionary<string, AmmoSetDef> turretsAmmoSet, StringBuilder log = null)
+		private static void LinkTurrets(VehicleDef vehicle, StringBuilder log = null)
 		{
+			//Vehicle has turrets at all
 			if (vehicle.GetCompProperties<CompProperties_VehicleTurrets>() is CompProperties_VehicleTurrets compTurrets)
 			{
 				//Several turrets can be attached to a single vehicle
 				foreach (VehicleTurret turret in compTurrets.turrets)
-				{
-					log?.Append("Turret: " + turret.turretDef);
-
-					//ammoSet exists for the turret
-					if (turretsAmmoSet[turret.turretDef.ToString()] != null)
-					{
-						log?.AppendLine(" - " + turretsAmmoSet[turret.turretDef.ToString()]);
-
-						AddHyperlinkAmmoSet(vehicle, turretsAmmoSet[turret.turretDef.ToString()]);
-					}
-				}
+					LinkTurret(vehicle, turret.turretDef, log);
 			}
 		}
 
 		/// <summary>
-		/// <c>AddHyperlinkAmmoSet</c> checks and adds new Hyperlink
+		/// <c>LinkTurret</c> adds hyperlink for the turret
 		/// </summary>
-		private static void AddHyperlinkAmmoSet(VehicleDef vehicle, AmmoSetDef ammoSet, StringBuilder log = null)
+		private static void LinkTurret(VehicleDef vehicle, VehicleTurretDef turretDef, StringBuilder log = null)
 		{
-			if (ammoSet == null)
-				return;
-
-			AddHyperlink(vehicle, ammoSet, log);
+			//Check if this turret has ammoSet defined for CE: DefModExtension
+			if (turretDef.HasModExtension<CETurretDataDefModExtension>())
+			{
+				Def ammoSet = turretDef.GetModExtension<CETurretDataDefModExtension>()._ammoSet;
+				log?.AppendLine("Turret: " + turretDef + " - " + ammoSet);
+				AddHyperlink(vehicle, ammoSet);
+			}
 		}
 
 		private static void AddHyperlink(Def def, Def linkToAdd, StringBuilder log = null)
 		{
-			if (def == null) return;
+			if (def == null || linkToAdd == null)
+				return;
 			if (def.descriptionHyperlinks == null)
 				def.descriptionHyperlinks = new List<DefHyperlink>();
-			foreach (DefHyperlink item in def.descriptionHyperlinks)
-			{
-				if (item.def == linkToAdd)
-				{
-					log?.AppendLine("Link duplicate: " + item.def);
-					return;
-				}
-			}
+			if (def.descriptionHyperlinks.Any(tmp => tmp.def == linkToAdd))
+				return;
+
 			def.descriptionHyperlinks.Add(linkToAdd);
-		}
-
-		/// <summary>
-		/// <c>MakeTurretList</c> goes through all turrets and creates dictionary ["defName"] = ammoSet
-		/// </summary>
-		private static Dictionary<string, AmmoSetDef> TurretsAmmoSetDict()
-		{
-			var dict = new Dictionary<string, AmmoSetDef>();
-
-			foreach (VehicleTurretDef turret in DefDatabase<VehicleTurretDef>.AllDefsListForReading)
-			{
-				//Check if this turret has ammoSet defined for CE: DefModExtension
-				if (turret.HasModExtension<CETurretDataDefModExtension>())
-				{
-					//well, it is not O(n^2), but we go through this array (list) for every turret. It is done only once... so...
-					foreach (AmmoSetDef ammoSet in DefDatabase<AmmoSetDef>.AllDefsListForReading)
-					{
-						if (ammoSet.defName == turret.GetModExtension<CETurretDataDefModExtension>().ammoSet)
-							dict[turret.defName] = ammoSet;
-					}
-				}
-				else
-					dict[turret.defName] = null;
-			}
-			return dict;
-		}
-
-		/// <summary>
-		/// <c>UpgradesSetDict</c> goes through all upgrades and creates dictionary ["defName"] = UpgradeTreeDef
-		/// </summary>
-		private static Dictionary<string, UpgradeTreeDef> UpgradesSetDict()
-		{
-			Dictionary<string, UpgradeTreeDef> dict = new Dictionary<string, UpgradeTreeDef>();
-
-			foreach (UpgradeTreeDef upgradeDef in DefDatabase<UpgradeTreeDef>.AllDefsListForReading)
-			{
-				dict[upgradeDef.defName] = upgradeDef;
-			}
-
-			return dict;
-		}
-
-		/// <summary>
-		/// Method <c>LogTurrets</c> display the Turret dictionary
-		/// </summary>
-		private static void LogTurrets(StringBuilder log, Dictionary<string, AmmoSetDef> turrets)
-		{
-			foreach (var turret in turrets)
-			{
-				if (turret.Value != null)
-					log?.Append("Turret: " + turret.Key + " - " + turret.Value.defName + "\n");
-				else
-					log?.Append("Turret: " + turret.Key + " - \n");
-			}
-
-			log?.Append("\n");
-		}
-
-		/// <summary>
-		/// <c>LogTurrets</c> displays the upgrade dictionary
-		/// </summary>
-		private static void LogUpgrades(StringBuilder log, Dictionary<string, UpgradeTreeDef> upgrades)
-		{
-			foreach (var upgrade in upgrades)
-			{
-				log.Append("Upgrade: " + upgrade.Key + " - " + upgrade.Value.defName + "\n");
-			}
-			log.Append("\n");
 		}
 	}
 }
